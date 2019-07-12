ed8e3ffbf871f7b32659da58fc92a93ba86d4e95
MasterGrayfur <josephavreed@gmail.com>
2019-07-12 09:51:03 -0300

    comit index.php

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+session_start();
+?>
+
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <meta charset="utf-8">
+
+</head>
+
+<body>
+
+    <?php if(empty($_SESSION["usuario"])){?>
+    
+    
+        
+
+
+		<div id="topo" class="jumbotron-fluid row">
+
+
+		<div id="topo_1" class="col-2">
+				<a href="index.php" id="linkpag">
+				<img id="logoIcon" src="visao/logo_short.png" alt="">
+				</a>
+			</div>
+			<div class="col-6" id="main">
+			<form class="form-inline" action="">
+				<input id="pesquisar" class="form-control mr-sm-2" type="text" placeholder="Pesquisar">
+
+			</form>
+			</div>
+
+			<div class="col-4 row" id="perfil">
+                <div class="col-2" id="fotoPerfil">
+                    <img id="icon" src="visao/basketball-player.png" alt="">
+                    </div>
+
+                <div class="col-10 row" id="teste">
+                    <div class="col-6" id="userName">
+                        <a href="login.php">entrar</a>
+                    </div>
+						
+                <div class="buttonBox col-6 fit">
+
+			<img src="visao/notification.png" width="30px" alt=""id="iconBar">
+			<a href="controle/sair.php"><img src="visao/exit_icon.png" width="30px" alt=""id="iconBar"></a>
+
+</div>
+			</div>
+			</div>
+			</div>
+			
+
+			
+		</div>
+
+
+    
+
+    <div class="container-fluid">
+        
+
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+                <div class="item active">
+                    <img src="visao/compartilhe.png" alt="Compartilhe suas jogadas e convide seus amigos pro baba!" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="visao/adicione_um_jogo.png" alt="Marque um baba onde você quiser!" style="width:100%;">
+                </div>
+        
+                <div class="item">
+                    <img src="visao/logo.png" alt="KD meu baba!" style="width:100%;">
+                </div>
+        </div>
+
+        <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+
+    
+    <?php } else{
+
+    header("location: visao/index.php");    
+    }
+    ?>
+
+
+
+</body>
+
+
+
+</html>
